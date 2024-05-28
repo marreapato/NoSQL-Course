c238a313f038e9794ad766be241230d33b1052b3
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-05-28 09:26:52 -0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,5 @@
+use cursoMongoDB
+
+db.atividades.insert({titulo:'Primeira Postagem',conteudo:'Conteudo 1',tags:[]})
+db.atividades.insert({titulo:'Segunda Postagem',conteudo:'Conteudo 2',tags:[]})
+db.atividades.insert({titulo:'Terceira Postagem',conteudo:'Conteudo 3',tags:[]})
