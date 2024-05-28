45f2edd6123f1437edabae99c7fb63c5775eef86
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-05-28 09:54:01 -0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,5 +1,14 @@
 use cursoMongoDB
 
-db.atividades.insert({titulo:'Primeira Postagem',conteudo:'Conteudo 1',tags:[]})
-db.atividades.insert({titulo:'Segunda Postagem',conteudo:'Conteudo 2',tags:[]})
-db.atividades.insert({titulo:'Terceira Postagem',conteudo:'Conteudo 3',tags:[]})
+db.atividades.remove({});//deleting all from collection
+
+db.atividades.insertOne({titulo:'Primeira Postagem',conteudo:'Conteudo 1',tags:[]});
+
+db.atividades.insertOne({titulo:'Segunda Postagem',conteudo:'Conteudo 2',tags:[]});
+
+db.atividades.insertOne({titulo:'Terceira Postagem',conteudo:'Conteudo 3',tags:[]});
+
+db.atividades.find();
+
+db.atividades.find().pretty();
+
