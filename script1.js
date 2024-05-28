66f9b90c5f4b346d7e09c8ce452cae31b277f840
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2024-05-28 12:43:30 -0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -2,11 +2,11 @@ use cursoMongoDB
 
 db.atividades.remove({});//deleting all from collection
 
-db.atividades.insertOne({titulo:'Primeira Postagem',conteudo:'Conteudo 1',tags:[]});
+db.atividades.insertOne({titulo:'Primeira Postagem',conteudo:'Conteudo 1',tags:{qtde:12}});
 
-db.atividades.insertOne({titulo:'Segunda Postagem',conteudo:'Conteudo 2',tags:[]});
+db.atividades.insertOne({titulo:'Segunda Postagem',conteudo:'Conteudo 2',tags:{qtde:9}});
 
-db.atividades.insertOne({titulo:'Terceira Postagem',conteudo:'Conteudo 3',tags:[]});
+db.atividades.insertOne({titulo:'Terceira Postagem',conteudo:'Conteudo 3',tags:{qtde:11}});
 
 db.atividades.find();
 
@@ -23,3 +23,16 @@ db.atividades.find({},{titulo:true});
 db.atividades.find({},{titulo:true,_id:false}).sort({titulo:1});
 
 db.atividades.findOne({titulo:"Primeira Postagem"})
+
+db.atividades.find({conteudo:"Conteudo 2"}).pretty();
+
+// filtro com In
+
+db.atividades.find({conteudo:{$in:["Conteudo 1","Conteudo 2"]}}).pretty();
+
+db.atividades.find({conteudo:{$in:["Conteudo 1","Conteudo 2","Conteudo 3"]},"tags.qtde":{$lt:12}}).pretty();
+
+//contagem
+
+db.atividades.count();
+
